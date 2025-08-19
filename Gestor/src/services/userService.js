dd998fdf411776cd982385e2a703be694f6bb18d
victor <vrubinr501@gmail.com>
2025-08-19 15:04:20 +0200

    19/08/2025

--- Gestor/src/services/userService.js
+++ Gestor/src/services/userService.js
@@ -39,3 +39,38 @@ export const registrar = async(userData) =>{
     }
 }
 
+/**
+ * Funcion para el login del usuario
+ * 
+ * @param {object} userData - El objeto es el usuario con el cual vamos a iniciar sesion
+ * @returns {Promise<object>} - Una promesa que resuleve con los datos del usuario si el registro es existoso
+ */
+
+export const login = async(userData) => {
+    const url = `${API_BASE_URL}/users/login`;
+    console.log("Intenando conectar a la url: ", url );
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+
+        //Verificamos si la respuesta es correcta
+        if (!response.ok){
+            const erroData = await response.json();
+            throw new Error(erroData ||'Error de credenciales');
+        }
+
+        const result = await response.json();
+        return result;
+    } catch(error){
+        //Relanzamos el error para que el componente lo pueda manera
+        console.error('Error en authService.registerUser:', error.message);
+        throw error;
+    }
+}
+
